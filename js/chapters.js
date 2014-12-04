eb0a0b14938a43c2e863c2942055cc93c3435456
Bhanu <bhanu@servelots.com>
2014-12-04 19:00:17 +0530

    added txt-anno

--- js/chapters.js
+++ js/chapters.js
@@ -2,6 +2,10 @@ window.chapters = [{el: "#chapter2", //  html id of the container of chapter
                     "who":"scribe",  //  the author of the swt
                     "what":"img-anno", // the type of swt
                     "tag": "lolcat"}, // tag by which swt have to be filtered
+                   {el: "#chapter2",
+                   "who" : "scribe",
+                   "what" : "txt-anno",
+                   "tag" : "lolcat" },
                    {el: "#chapter3",
                     "who":"scribe",
                     "what":"img-anno",
@@ -13,4 +17,8 @@ window.chapters = [{el: "#chapter2", //  html id of the container of chapter
                    {el: "#story-content",
                     "who":"scribe",
                     "what":"img-anno",
+                    "tag": "lolcat"},
+                   {el: "#story-content",
+                    "who":"scribe",
+                    "what":"txt-anno",
                     "tag": "lolcat"}];
