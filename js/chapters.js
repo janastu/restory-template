4f48b7b48ed14501231a0e8b2de1f49b8300faca
Arvind <arvindkhadri@gmail.com>
2014-12-04 18:08:59 +0530

    Add chapters.js

---
+++ js/chapters.js
@@ -0,0 +1,16 @@
+window.chapters = [{el: "#chapter2", //  html id of the container of chapter
+                    "who":"scribe",  //  the author of the swt
+                    "what":"img-anno", // the type of swt
+                    "tag": "lolcat"}, // tag by which swt have to be filtered
+                   {el: "#chapter3",
+                    "who":"scribe",
+                    "what":"img-anno",
+                    "tag": "lolcat"},
+                   {el: "#chapter4",
+                    "who":"scribe",
+                    "what":"img-anno",
+                    "tag": "lolcat"},
+                   {el: "#story-content",
+                    "who":"scribe",
+                    "what":"img-anno",
+                    "tag": "lolcat"}];
